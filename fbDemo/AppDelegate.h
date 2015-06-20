c5c7267e18ce9c7f252a5795423b527c1af35409
Nikhil Wali <nikhilwali@Nikhils-MacBook-Pro.local>
2015-06-20 15:59:31 +0530

    Initial Commit

---
+++ fbDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbDemo
+//
+//  Created by Nikhil Wali on 20/06/15.
+//  Copyright (c) 2015 Nikhil Wali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fbDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fbDemo
+//
+//  Created by Nikhil Wali on 20/06/15.
+//  Copyright (c) 2015 Nikhil Wali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
